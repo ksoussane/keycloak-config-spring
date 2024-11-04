cddd397ac4619232fb2927c7d0744a93aac5dcff
khalil-soussane-3 <soussanekhalil9@gmail.com>
2024-11-04 19:17:42 +0100

    first commit

---
+++ src/main/java/com/incidentManagement/DemoController.java
@@ -0,0 +1,25 @@
+package com.incidentManagement;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/api/v1/demo")
+public class DemoController {
+
+    @GetMapping
+    @PreAuthorize("hasRole('client_user')")
+    public String hello() {
+        return "Hello from Spring boot & Keycloak";
+    }
+
+    @GetMapping("/hello-2")
+    @PreAuthorize("hasRole('client_admin')")
+    public String hello2() {
+        return "Hello from Spring boot & Keycloak - ADMIN";
+    }
+}
---
+++ src/main/java/com/incidentManagement/keycloakConfig.java
@@ -0,0 +1,13 @@
+package com.incidentManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class keycloakConfig {
+
+	public static void main(String[] args) {
+		SpringApplication.run(keycloakConfig.class, args);
+	}
+
+}
